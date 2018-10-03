2124611d503fa41e1cc040949046bf301354d980
Naik <yogini.naik@capgemini.com>
2018-10-03 16:02:11 +0530

    maven project added

---
+++ CalculatorApp/src/test/java/com/cg/maven/test/TestCalculator.java
@@ -0,0 +1,27 @@
+package com.cg.maven.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.cg.demo.Calculator;
+
+public class TestCalculator {
+
+
+	@Test
+	public void testAdd(){
+		Calculator c1= new Calculator();
+		int res= c1.add(3, 6);
+		Assert.assertEquals(9, res);
+	}
+	
+	@Test
+	public void testSubtract(){
+		Calculator c1= new Calculator();
+		int res= c1.subtract(10, 6);
+		Assert.assertEquals(4, res);
+	}
+	
+	
+}

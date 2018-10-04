ce52ed52a4592ace3d49f32c068f42d63d98293c
Naik <yogini.naik@capgemini.com>
2018-10-04 13:10:36 +0530

    file chaged

--- CalculatorApp/src/main/java/com/cg/demo/Calculator.java
+++ CalculatorApp/src/main/java/com/cg/demo/Calculator.java
@@ -3,15 +3,22 @@ package com.cg.demo;
 public class Calculator {
 
 	public int add(int a,int b){
+		int total;
 		return a+b;
 	}
 	public int subtract(int a,int b){
-		return a-b;
+	System.out.println("hello");	
+	return a-b;
 	}
 	public int multiply(int a,int b){
-		return a*b;
+				
+	return a*b;
 	}
 	public double divide(int a,int b){
 		return (double)a/b;
 	}
+	public static void main(String args[]) throws Exception{
+	System.out.println("welcome to DevOps");
+}
+
 }
